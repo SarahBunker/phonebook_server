08c965e51262a99cc74eaed9ed80cf598796592b
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-18 10:50:42 -0700

    implmenting a server for phonebook project

---
+++ index.js
@@ -0,0 +1,117 @@
+"use strict"
+
+const express = require('express');
+const app = express();
+var morgan = require('morgan')
+
+app.use(express.json());
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+// app.use(morgan('tiny'));
+
+let persons = [
+    {
+      "id": 1,
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": 2,
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": 3,
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": 4,
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+  let text = `
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>
+  `
+  response.send(text);
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+function getRndInteger(min, max) {
+  // where min and max are both included
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function generateId() {
+  return getRndInteger(1, 10000);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  let namesInPhonebook = persons.map(person => person.name);
+  if (namesInPhonebook.includes(body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  persons = persons.concat(person);
+
+  response.json(person);
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
