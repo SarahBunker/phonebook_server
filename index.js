19c707c506b46b6a9a5be1026fd4c11ea847c9ec
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-18 18:39:13 -0700

    deploying changes to the fly

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const app = express();
 var morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json());
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 // app.use(morgan('tiny'));
+
+
+app.use(cors())
 
 let persons = [
     {
@@ -111,7 +115,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
